a9bf78e51c56b64096dacec2e4dc8da42f1a14da
Robert Buels <rmb32@cornell.edu>
2010-09-17 19:52:02 -0700

    need to pass the stash to forward_to_mason_view under CGIs also

--- lib/SGN/Role/Site/Mason.pm
+++ lib/SGN/Role/Site/Mason.pm
@@ -68,8 +68,8 @@ sub forward_to_mason_view {
     my ($comp,@args) = @_;
 
     if( $ENV{SERVER_SOFTWARE} =~ /HTTP-Request-AsCGI/ ) {
-        my @args = @_;
-        $self->_trap_mason_error( sub { $self->_mason_interp->exec( @args ) } );
+        my ($view,@args) = @_;
+        $self->_trap_mason_error( sub { $self->_mason_interp->exec( $view, %{$self->stash}, @args ) } );
         die ["EXIT\n",0]; #< weird thing for working with Catalyst's CGIBin controller
     } else {
         $self->stash->{template} = $comp;
