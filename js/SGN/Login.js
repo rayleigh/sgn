ffd6d074a5374066ab4c1939df833e3c474c3c30
Robert Buels <rmb32@cornell.edu>
2010-09-17 17:33:26 -0700

    tweak login form javascript, looks better

--- js/SGN/Login.js
+++ js/SGN/Login.js
@@ -7,14 +7,12 @@ if(!SGN.Login) SGN.Login = {
 
 jQuery(function($) { 
     $('#login_control div.logged_out a.login').click(function() {
-        $('#login_control div.logged_out').hide();
-        $('#login_control form.login_form').show();
+        $('#login_control form.login_form').slideDown();
         return false;
     });
 
     $("#login_control form.login_form input[value='cancel']").click(function() {
-        $('#login_control form.login_form').hide();
-        $('#login_control div.logged_out').show();
+        $('#login_control form.login_form').slideUp();
         return false;
     });
 });
