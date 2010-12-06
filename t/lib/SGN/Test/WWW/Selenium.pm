f35d77a46341ca9b6768d2f841207a6977648663
Jonathan "Duke" Leto <jonathan@leto.net>
2010-12-06 12:49:15 -0800

    Forgot to add SGN::Test::WWW::Selenium

---
+++ t/lib/SGN/Test/WWW/Selenium.pm
@@ -0,0 +1,18 @@
+package SGN::Test::WWW::Selenium;
+
+use Modern::Perl;
+use Test::WWW::Selenium;
+
+sub new {
+    my ($c, %opts);
+    return Test::WWW::Selenium->new(
+        host        => ( $ENV{SELENIUM_HOST}      || 'winsel.sgn.cornell.edu' ),
+        port        => ( $ENV{SELENIUM_HOST_PORT} || 4444 ),
+        browser     => ( $ENV{SELENIUM_BROWSER}   || '*iexplorer' ),
+        browser_url => ( $ENV{SGN_TEST_SERVER} || 'localhost:3000' ),
+        %opts,
+    );
+}
+
+
+1;
