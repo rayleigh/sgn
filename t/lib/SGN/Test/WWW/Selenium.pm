7512741469f8457554af170b97bb3cf9eaed5610
Jonathan "Duke" Leto <jonathan@leto.net>
2010-12-07 14:47:16 -0500

    Fix SGN::Test::WWW::Selenium's default browser and improve a basic selenium test

--- t/lib/SGN/Test/WWW/Selenium.pm
+++ t/lib/SGN/Test/WWW/Selenium.pm
@@ -8,7 +8,7 @@ sub new {
     return Test::WWW::Selenium->new(
         host        => ( $ENV{SELENIUM_HOST}      || 'winsel.sgn.cornell.edu' ),
         port        => ( $ENV{SELENIUM_HOST_PORT} || 4444 ),
-        browser     => ( $ENV{SELENIUM_BROWSER}   || '*iexplorer' ),
+        browser     => ( $ENV{SELENIUM_BROWSER}   || '*iexplore' ),
         browser_url => ( $ENV{SGN_TEST_SERVER} || 'localhost:3000' ),
         %opts,
     );
